a85b510bf0671564c7d07e1f603efec7a0e88303
Valerie <valerieloveland@gmail.com>
2021-05-12 11:15:02 -0400

    finished first assignment

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -9,8 +9,9 @@ export function ExpenseDate(props) {
     <div className="expense-date">
       <div className="expense-date__month">{month}</div>
       <div className="expense-date__year">{year}</div>
-
       <div className="expense-date__day">{day}</div>
     </div>
   );
 }
+
+export default ExpenseDate;
